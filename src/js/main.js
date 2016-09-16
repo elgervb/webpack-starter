9b6345510acb27b220911c1e97503b5d974a4cc3
Elger van Boxtel <elgervb@users.noreply.github.com>
2016-09-16 16:05:36 +0200

    #1 counter page for e2e tests
    fixed #1

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,17 @@
 import Counter from './components/counter';
 
-const counter = new Counter();
-counter.increase();
+const display = document.querySelector('.counter__display');
+const increment = document.querySelector('.counter__action--increment');
+const decrement = document.querySelector('.counter__action--decrement');
+
+if (display && increment && decrement) {
+    const counter = new Counter();
+
+    increment.addEventListener('click', () => {
+        display.innerText = counter.increase();
+    });
+
+    decrement.addEventListener('click', () => {
+        display.innerText = counter.decrease();
+    });
+}
