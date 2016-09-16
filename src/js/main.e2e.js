26e233faf5169b8242ef2acd81ca1ac43f6b437f
Elger van Boxtel <elgervb@users.noreply.github.com>
2016-09-16 13:56:19 +0200

    #4 webdriverIO e2e tests
    fixed #4

---
+++ src/js/main.e2e.js
@@ -0,0 +1,18 @@
+import assert from 'assert';
+
+describe('webdriver.io page', () => {
+    it('should have a page title', () => {
+        browser.url('/');
+        const title = browser.getTitle();
+        assert.equal(title, 'Webpack starter');
+    });
+
+    it('should have a title', () => {
+        assert.equal(browser.element('.header1').getText(), 'Webpack Starter');
+    });
+
+    it('should have an image', () => {
+        assert.equal(browser.element('img').getAttribute('src'), 'http://localhost:4000/assets/img/webpack.svg');
+        assert.equal(browser.element('img').getAttribute('alt'), 'WebPack');
+    });
+});
